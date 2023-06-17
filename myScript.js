464dda9229e27caaac89b76946f681309e37606b
dvcorreia1984 <victor@onsleer.co.za>
2023-06-17 12:11:21 +0200

    Added Javascript file

---
+++ myScript.js
@@ -0,0 +1,10 @@
+function openMenu() {
+  var menu = document.getElementById("menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+
