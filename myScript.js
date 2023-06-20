82a574777722056031ad4687f2c02707e543646c
dvcorreia1984 <victor@onsleer.co.za>
2023-06-20 12:02:56 +0200

    ESLint Errors fixed

--- myScript.js
+++ myScript.js
@@ -1,17 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100vw";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById('mySidenav').style.width = '100vw';
+  document.getElementById('main').style.marginLeft = '250px';
 }
 
+// eslint-disable-next-line no-unused-vars
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById('mySidenav').style.width = '0';
+  document.getElementById('main').style.marginLeft = '0';
 }
-
-
-
-
-
-
-
-
