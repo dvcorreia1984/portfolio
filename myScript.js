1c7b3bb973bd80a40d54d4f385296b7f0fe9aed2
dvcorreia1984 <victor@onsleer.co.za>
2023-06-26 15:47:23 +0200

    Add comment to Javascript file

--- myScript.js
+++ myScript.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 const openNav = function openNav() {
   document.getElementById('mySidenav').style.width = '100vw';
   document.getElementById('main').style.marginLeft = '250px';
 }
 
+// eslint-disable-next-line no-unused-vars
 const closeNav = function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
