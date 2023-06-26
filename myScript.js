dbe677bdbad289957a312949dd283a6ca18c892f
dvcorreia1984 <victor@onsleer.co.za>
2023-06-26 18:21:20 +0200

    Fixed ESLint error in myScript.js file

--- myScript.js
+++ myScript.js
@@ -1,9 +1,15 @@
+const menuButton = document.getElementById('menu');
+const mySidenav = document.getElementById('mySidenav');
+
 function openNav() {
-    const mySidenav = document.getElementById("mySidenav");
-    mySidenav.style.width = "100vw";
+  const mySidenav = document.getElementById('mySidenav');
+  mySidenav.style.width = '100vw';
 }
 
 function closeNav() {
-    const mySidenav = document.getElementById("mySidenav");
-    mySidenav.style.width = "0";
+  const mySidenav = document.getElementById('mySidenav');
+  mySidenav.style.width = '0';
 }
+
+menuButton.addEventListener('click', openNav);
+mySidenav.addEventListener('click', closeNav);
