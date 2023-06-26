759f9eec5c3fdee4bb0d86ad301170acc7e9753e
dvcorreia1984 <victor@onsleer.co.za>
2023-06-26 15:50:44 +0200

    Added semicolons

--- myScript.js
+++ myScript.js
@@ -2,10 +2,10 @@
 const openNav = function openNav() {
   document.getElementById('mySidenav').style.width = '100vw';
   document.getElementById('main').style.marginLeft = '250px';
-}
+};
 
 // eslint-disable-next-line no-unused-vars
 const closeNav = function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
-}
+};
