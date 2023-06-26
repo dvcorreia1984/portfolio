0cd8d74625d67ac7051775d57bc37ece2ad7700a
dvcorreia1984 <victor@onsleer.co.za>
2023-06-26 15:43:22 +0200

    Edit myscript.js file

--- myScript.js
+++ myScript.js
@@ -1,9 +1,9 @@
-function openNav() {
+const openNav = function openNav() {
   document.getElementById('mySidenav').style.width = '100vw';
   document.getElementById('main').style.marginLeft = '250px';
 }
 
-function closeNav() {
+const closeNav = function closeNav() {
   document.getElementById('mySidenav').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
 }
