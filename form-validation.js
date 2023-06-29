cec201bd3f19564e4eddb08d3abdd7374d28b2a2
dvcorreia1984 <victor@onsleer.co.za>
2023-06-29 19:15:10 +0200

    Added code for form validation in form-validation.js, html and css files

--- form-validation.js
+++ form-validation.js
@@ -1,17 +1,55 @@
+const form = document.getElementById('form');
+const fullname = document.getElementById('fullname');
+const email = document.getElementById('email');
+const message = document.getElementById('msg');
 
-const email = document.getElementById('mail');
+function checkInputs() {
+  // trim to remove the whitespaces
+  const nameValue = fullname.value.trim();
+  const emailValue = email.value.trim();
+  const msgValue = message.value.trim();
 
-email.addEventListener('input', (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity('I am expecting an email address!');
-  } else {
-    email.setCustomValidity('');
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
   }
 
-  // Add lowercase validation
-  if (email.value !== email.value.toLowerCase()) {
-    email.setCustomValidity('Email address must be lowercase!');
-  } else {
-    email.setCustomValidity('');
+  function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
   }
+
+  function isEmail(email) {
+    return /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/.test(email);
+  }
+
+  if (nameValue === '') {
+    setErrorFor(fullname, 'Name cannot be blank');
+  } else {
+    setSuccessFor(fullname);
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+  } else if (emailValue !== emailValue.toLowerCase()) {
+    setErrorFor(email, 'Email must be lowercase');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (msgValue === '') {
+    setErrorFor(message, 'Message cannot be blank');
+  } else {
+    setSuccessFor(message);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  checkInputs();
 });
