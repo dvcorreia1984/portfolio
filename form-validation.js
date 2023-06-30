bad41ab2f637c65acb74b60aead1b4edcd261916
dvcorreia1984 <victor@onsleer.co.za>
2023-06-30 15:53:01 +0200

    Updated local storage part of form-validation.js file

--- form-validation.js
+++ form-validation.js
@@ -1,25 +1,21 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
-const errorMessage = document.getElementById('error-message');
+
+const formDataObject = {};
 
 form.addEventListener('submit', (e) => {
-  if (email.value !== email.value.toLowerCase()) {
-    e.preventDefault();
-    errorMessage.textContent = 'Email must be lowercase';
-  } else {
-    errorMessage.textContent = '';
-  }
-  errorMessage.style.color = 'red';
+  const myformData = new FormData(e.target);
+  myformData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+  const formData = JSON.stringify(formDataObject);
+  localStorage.setItem('Data', formData);
 });
 
-// Store email in local storage
-const email = document.getElementById('email');
-const form = document.getElementById('form');
+const userData = JSON.parse(localStorage.getItem('Data'));
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  localStorage.setItem('email', email.value);
-  form.reset();
-}
-
-
+window.onload = () => {
+  email.value = userData.email;
+  document.getElementById('fullname').value = userData.fullName;
+  document.getElementById('message').value = userData.message;
+};
